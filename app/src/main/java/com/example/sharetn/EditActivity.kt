2fe224a454c16e951f808a0fa8d33a50c32d3ba8
harutiro <harutiro2027@outlook.jp>
2021-06-19 18:43:18 +0900

    add: テキスト取得、faviconの取得等完成

--- app/src/main/java/com/example/sharetn/EditActivity.kt
+++ app/src/main/java/com/example/sharetn/EditActivity.kt
@@ -1,11 +1,97 @@
 package com.example.sharetn
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.sharetn.dousa.UrlDomein
+import com.squareup.picasso.Picasso
 
 class EditActivity : AppCompatActivity() {
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        var comeText = ""
+        var domein = ""
+        var title = ""
+
+
+
+
+//=============================共有で飛ばされたときに動く部分
+        if (TextUtils.equals(intent.action, Intent.ACTION_SEND)) {
+            val extras = intent.extras
+            val extraText = extras!!.getCharSequence(Intent.EXTRA_TEXT).toString()
+
+//           URLや文字の受け取り
+            comeText = extraText
+            domein = UrlDomein().hen(comeText)
+
+
+//===========================URLで動く部分
+            if(UrlDomein().check(comeText)) {
+
+//==============================faviconの取得
+                Picasso.get()
+                    //いらすとやの画像URL
+                    .load("https://www.google.com/s2/favicons?domain=$domein")
+                    .resize(300, 300) //表示サイズ指定
+                    .centerCrop() //resizeで指定した範囲になるよう中央から切り出し
+                    .into(findViewById<ImageView>(R.id.imageView)) //imageViewに流し込み
+
+
+//================================WebViewでのタイトルの取得の仕方。
+                val webview = WebView(this)
+
+//              URL
+                webview.loadUrl(comeText)
+
+//              画面を取得するときにここがないと取得できない
+                val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                val params = WindowManager.LayoutParams(
+                    300,
+                    300,
+                    WindowManager.LayoutParams.TYPE_APPLICATION,
+                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                    PixelFormat.TRANSLUCENT
+                )
+
+                wm.addView(webview, params)
+
+                //非表示
+                webview.visibility = View.GONE
+
+//                 画像取得部分
+                webview.webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView, url: String) {
+
+                        title = webview.title.toString()
+
+
+
+
+
+                    }
+
+                }
+            }
+        }
+
+
+
+
+
+
     }
 }
