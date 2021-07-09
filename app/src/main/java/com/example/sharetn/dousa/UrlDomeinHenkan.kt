3ad7d1212f76627a6da9df447f8e4e297744ab67
harutiro <harutiro2027@outlook.jp>
2021-07-09 17:32:03 +0900

    fix: Urlの変換部分少し見やすく変更した。

--- app/src/main/java/com/example/sharetn/dousa/UrlDomeinHenkan.kt
+++ app/src/main/java/com/example/sharetn/dousa/UrlDomeinHenkan.kt
@@ -16,106 +16,60 @@ class UrlDomein{
 
                 'h' -> {
                     if(httpsCheck == ""){
-
                         httpsCheck += ch
-
                     }else{
-
                         if(surassyu == "//"){
                             domein += ch
                         }
-
                     }
-
                 }
                 't' ->{
-
                     if(httpsCheck == "h"){
-
                         httpsCheck += ch
-
                     }else if(httpsCheck == "ht") {
-
                         httpsCheck += ch
-
                     }else{
-
                         if(surassyu == "//"){
                             domein += ch
                         }
-
                     }
-
                 }
                 'p' ->{
                     if(httpsCheck == "htt"){
-
                         httpsCheck += ch
-
                     }else{
-
                         if(surassyu == "//"){
                             domein += ch
                         }
-
                     }
-
                 }
                 's' ->{
                     if(httpsCheck == "http" && surassyu ==""){
-
                         httpsCheck += ch
-
                     }else{
-
                         if(surassyu == "//"){
                             domein += ch
                         }
-
                     }
-
                 }
-
                 '/' ->{
                     if((httpsCheck == "https" || httpsCheck == "http") && (surassyu == "" || surassyu == "/")){
-
                         surassyu += ch
-
                     }else{
-
                         if(surassyu == "//"){
                             break
                         }
-
                     }
-
                 }
-
                 ':' ->{}
-
                 else ->{
                     if(httpsCheck == "https" || httpsCheck == "http"){
-
                         domein += ch
-
-                    }else{
-
                     }
                 }
-
-
             }
-
-
-
-//            https://www.google.com/s2/favicons?domain=
-
-
         }
-
-
         return domein
-
     }
 
     fun check(moto: String): Boolean {
@@ -129,80 +83,41 @@ class UrlDomein{
         for (ch in charAry){
 
             when (ch){
-
                 'h' -> {
                     if(httpsCheck == ""){
-
                         httpsCheck += ch
-
                     }
-
                 }
                 't' ->{
-
                     if(httpsCheck == "h"){
-
                         httpsCheck += ch
-
                     }else if(httpsCheck == "ht") {
-
                         httpsCheck += ch
-
                     }
-
                 }
                 'p' ->{
                     if(httpsCheck == "htt"){
-
                         httpsCheck += ch
-
                     }
-
                 }
                 's' ->{
                     if(httpsCheck == "http" && surassyu ==""){
-
                         httpsCheck += ch
-
                     }
-
                 }
-
                 '/' ->{
                     if((httpsCheck == "https" || httpsCheck == "http") && (surassyu == "" )){
-
                         surassyu += ch
-
                     }else if(surassyu == "/"){
-
                         surassyu += ch
                         break
-
                     }
-
                 }
-
                 ':' ->{}
-
                 else ->{
-
                 }
-
-
             }
-
-
-
-
-
-//            https://www.google.com/s2/favicons?domain=
-
-
         }
-
         return (httpsCheck == "https" || httpsCheck == "http") && (surassyu == "//" )
-
-
-
     }
 }
