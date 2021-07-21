ce98c237096b161dd352a684731338aacdb86c62
harutiro <harutiro2027@outlook.jp>
2021-07-21 19:56:14 +0900

    add: viewActivityの作成

---
+++ app/src/main/java/com/example/sharetn/ViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sharetn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view)
+    }
+}
