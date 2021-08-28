8163a98be8c864abb6504a3cd6cf93ade5fb402e
harutiro <harutiro2027@outlook.jp>
2021-08-28 09:11:21 +0900

    fix: Viewアクティビティの消去

--- app/src/main/java/com/example/Clips/MainActivity.kt
+++ app/src/main/java/com/example/Clips/MainActivity.kt
@@ -61,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         adapter = MainRecyclerViewAdapter(this , object: MainRecyclerViewAdapter.OnItemClickListner{
             override fun onItemClick(item: MainDate) {
                 // SecondActivityに遷移するためのIntent
-                val intent = Intent(applicationContext, ViewActivity::class.java)
+                val intent = Intent(applicationContext, EditActivity::class.java)
                 // RecyclerViewの要素をタップするとintentによりSecondActivityに遷移する
                 // また，要素のidをSecondActivityに渡す
                 intent.putExtra("id", item.id)
